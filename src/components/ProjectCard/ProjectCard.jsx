fbcca0fc517eadd8efeacfdee15b29d1eb210d5a
Home <chris.wakley666@gmail.com>
2022-12-19 22:07:07 +0000

    styling changes complete. media queries next

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,53 +1,61 @@
 import "./ProjectCardStyling/ProjectCardMain.scss";
 import React, { useState } from "react";
 import Button from "../Button/Button";
-import BlackCross from "../../assets/images/black-cross.png";
+// import BlackCross from "../../assets/images/black-cross.png";
+import WhiteCross from "../../assets/images/white-cross.png";
 
 const ProjectCard = ({ name, image_url, link, language, description }) => {
-  const [showMore, setShowMore] = useState(true);
+    const [showMore, setShowMore] = useState(true);
 
-  const handleClick = () => {
-    setShowMore(!showMore);
-  };
+    const handleClick = () => {
+        setShowMore(!showMore);
+    };
 
-  const cardFaceJSX = (
-    <div className="card__content card__content--face">
-      <div className="card__imageContainer">
-        <img src={image_url} alt="project" className="card__image" />
-      </div>
-      <div className="card__body">
-        <p className="card__body card__body--language">{language}</p>
-        <h1 className="card__body card__body--name"> {name} </h1>
-        <div className="card__button--face">
-          <Button
-            buttonClass="button"
-            buttonText="See More"
-            handleClick={handleClick}
-          />
+    const cardFaceJSX = (
+        <div className='card__content card__content--face'>
+            <div className='card__imageContainer'>
+                <img src={image_url} alt='project' className='card__image' />
+            </div>
+            <div className='card__body'>
+                <p className='card__body card__body--language'>{language}</p>
+                <h1 className='card__body card__body--name'> {name} </h1>
+                <div className='card__button--face'>
+                    <Button
+                        buttonClass='buttonLarge'
+                        buttonText='See More'
+                        handleClick={handleClick}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-  const cardInfoJSX = (
-    <div className="card__content card__content--info">
-      <img
-        src={BlackCross}
-        alt="close"
-        className="card__cross"
-        onClick={handleClick}
-      />
-      <div className="card__description">
-        <p className="card__description--text"> {description} </p>
-      </div>
-      <div className="card__link">
-        <a href={link} target="_blank" rel="noreferrer">
-          <Button buttonClass="buttonLarge" buttonText="Repository Link" />
-        </a>
-      </div>
-    </div>
-  );
+    );
+    const cardInfoJSX = (
+        <div className='card__content card__content--info'>
+            <img
+                src={WhiteCross}
+                alt='close'
+                className='card__cross'
+                onClick={handleClick}
+            />
+            <div className='card__description'>
+                <p className='card__description--text'> {description} </p>
+            </div>
+            <div className='card__body'>
+                <p className='card__body card__body--language'>{language}</p>
+                <h1 className='card__body card__body--name'> {name} </h1>
+                <div className='card__link'>
+                    <a href={link} target='_blank' rel='noreferrer'>
+                        <Button
+                            buttonClass='buttonLarge'
+                            buttonText='Repository Link'
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 
-  return <div className="card">{showMore ? cardFaceJSX : cardInfoJSX}</div>;
+    return <div className='card'>{showMore ? cardFaceJSX : cardInfoJSX}</div>;
 };
 
 export default ProjectCard;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,18 +1,71 @@
 import "./AboutStyling/AboutMain.scss";
 import armyOne from "../../assets/images/armyone.jpg";
 import Snowboarding from "../../assets/images/Snowboarding.jpeg";
+import armyThree from "../../assets/images/armyThree.jpg";
+import armyFour from "../../assets/images/armyFour.jpg";
 import reme from "../../assets/images/reme.png";
 
 const About = () => {
     return (
         <div className='about'>
             <div className='about__bodyContainer'>
-                <div className='about__imageOne'>
-                    <img
-                        src={armyOne}
-                        alt='army'
-                        className='about__imageOne about__imageOne--armyOne'
-                    />
+                <div className='about__imageContainer'>
+                    <div className='about__imageOne'>
+                        <img
+                            src={armyOne}
+                            alt='army'
+                            className='about__imageOne about__imageOne--armyOne'
+                        />
+                        <div className='about__reme'>
+                            <img
+                                src={reme}
+                                alt='remelogo'
+                                className='about__reme'
+                            />
+                        </div>
+                    </div>
+                    <div className='about__imageTwo'>
+                        <img
+                            src={Snowboarding}
+                            alt='army'
+                            className='about__imageTwo about__imageTwo--armyTwo'
+                        />
+                        <div className='about__reme'>
+                            <img
+                                src={reme}
+                                alt='remelogo'
+                                className='about__reme'
+                            />
+                        </div>
+                    </div>
+                    <div className='about__imageThree'>
+                        <img
+                            src={armyThree}
+                            alt='army'
+                            className='about__imageThree about__imageThree--armyThree'
+                        />
+                        <div className='about__reme'>
+                            <img
+                                src={reme}
+                                alt='remelogo'
+                                className='about__reme'
+                            />
+                        </div>
+                    </div>
+                    <div className='about__imageFour'>
+                        <img
+                            src={armyFour}
+                            alt='army'
+                            className='about__imageFour about__imageFour--armyFour'
+                        />
+                        <div className='about__reme'>
+                            <img
+                                src={reme}
+                                alt='remelogo'
+                                className='about__reme'
+                            />
+                        </div>
+                    </div>
                 </div>
                 <div className='about__body'>
                     <p className='about__body about__body--text'>
@@ -31,7 +84,7 @@ const About = () => {
                         so this seems like a natural transition for me.
                         <br />
                         <br />
-                        Aside from my military background, I'd always been
+                        Aside from my military background, I've always been
                         hugely into adrenaline sports. I've represented the
                         British Army in Snowboarding, Operational Shooting and
                         Rock climbing against other nations armies at high level
@@ -43,18 +96,6 @@ const About = () => {
                         time into my family life. I occasionally enjoy reading a
                         good book as well.
                     </p>
-                    <img
-                        src={reme}
-                        alt='remelogo'
-                        className='about__body about__body--image'
-                    />
-                </div>
-                <div className='about__imageTwo'>
-                    <img
-                        src={Snowboarding}
-                        alt='army'
-                        className='about__imageTwo about__imageTwo--armyTwo'
-                    />
                 </div>
             </div>
         </div>
